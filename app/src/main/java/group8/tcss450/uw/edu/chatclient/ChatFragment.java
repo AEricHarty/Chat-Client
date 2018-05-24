4e2e7cb0416f5da7fbafe667ab333bb9b7bb5bbb
Lloyd <lloydb3@uw.edu>
2018-05-24 09:08:56 -0700

    Added constraints to chatFragment scroll view. and fixed chatFragment duplicating messages when screen is sycled on and off while in chatFragment.

--- app/src/main/java/group8/tcss450/uw/edu/chatclient/ChatFragment.java
+++ app/src/main/java/group8/tcss450/uw/edu/chatclient/ChatFragment.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -160,6 +161,7 @@ public class ChatFragment extends Fragment {
     @Override
     public void onResume() {
         super.onResume();
+        mOutputTextView.setText("");
         mListenManager.startListening();
     }
 
@@ -360,6 +362,8 @@ public class ChatFragment extends Fragment {
                     mOutputTextView.append(msg);
                     mOutputTextView.append(System.lineSeparator());
                 }
+                ScrollView scrollView = (ScrollView) getActivity().findViewById(R.id.chatOutputScrollView);
+                scrollView.fullScroll(ScrollView.FOCUS_DOWN);
             });
         }
     }
